378f016a78df1e611b3e31d178136137588774aa
schroedere16 <36168890+schroedere16@users.noreply.github.com>
2018-09-10 16:24:39 -0400

    Add files via upload

---
+++ Simple Blink/simpleblink2553/main.c
@@ -0,0 +1,19 @@
+#include <msp430.h> 
+#define BIT0 0x01;
+
+/**
+ * main.c
+ */
+int main(void)
+{
+	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
+	P1SEL &= ~BIT0;     //changes the P1SEL to zero
+	P1SEL2 &= ~BIT0;    //changes the P2SEL to zero
+	P1DIR |= BIT0;      //changes the least significant bit to be one to make direction register go to output
+	while(1)
+	{
+	    P1OUT ^= BIT0;  // changes the least significant bit of output to cyclle between 1 and 0 turning LED on and off
+	    __delay_cycles(100000); //delays the light switching allowing it to be seen
+	}
+	return 0;
+}
